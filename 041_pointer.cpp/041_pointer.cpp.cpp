5cd49659f8b3392b6d51520b0ee43c12cae26628
ihsan putra riadi <iputrariadi@gmail.com>
2023-05-10 15:13:19 +0700

    mengimplementasikan method luar class

--- 041_pointer.cpp/041_pointer.cpp.cpp
+++ 041_pointer.cpp/041_pointer.cpp.cpp
@@ -1,9 +1,28 @@
-#include <iostrream>
+#include <iostream>
 using namespace std;
 
 class mahasiswa {
 public:
 	int nim;
-	void shownim(); //deklarasi method
+	void showNim(); //deklarasi method 
 };
 
+void mahasiswa::showNim() { //implementasi method diluar class
+	cout << " No Induk = " << nim << endl;
+}
+
+int main()
+{
+	mahasiswa mhs{ 1 }; // object mhs 
+	mhs.showNim();      // Member Access Operator
+
+	mahasiswa& ref = mhs;  //Pointer Reference refMhs
+	ref.nim = 2;           //Member Acces Operator
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs;     //Pointer Dereference pMhs
+	pMhs->nim = 3;              //Arrow operator
+	pMhs->showNim();
+	system("pause");
+	return 0;
+}
