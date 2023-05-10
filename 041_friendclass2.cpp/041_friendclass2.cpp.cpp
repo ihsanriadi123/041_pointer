2288e6efae2746af9730619d4fd26864d38267d6
ihsan putra riadi <iputrariadi@gmail.com>
2023-05-10 16:16:27 +0700

    menambahakan data private nilai tersebut

---
+++ 041_friendclass2.cpp/041_friendclass2.cpp.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class pelajar;
+
+class manusia {
+public:
+	void shownilaipelajar(pelajar&);
+};
+
+class pelajar {
+private:
+	int nilai;
+public:
+	pelajar() {
+		nilai = 100;
+	}
+	friend void manusia::shownilaipelajar(pelajar& x);
+};
